dd9dc5a29061ea9b3a92a78e611ce4e7875e2fc3
Rashford <mikearowo@gmail.com>
2023-07-18 21:34:53 +0100

    new

--- hardhat/contracts/CryptoDevsNFT.sol
+++ hardhat/contracts/CryptoDevsNFT.sol
@@ -3,3 +3,13 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+cocntract CryptoDevsNFT is ERC721Enumerable {
+     // Initialize the ERC-721 contract
+     constructor() ERC721("CryptoDevs", "CD") {}
+
+     // Have a public mint function anyone can call to get an NFT
+     function mint() public {
+        _safeMint(msg.sender, totalSupply());
+     }
+}
---
+++ hardhat/contracts/FakeNFTMarketplace.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract FakeNFTMarketplace {
+    /// @dev Maintain a mapping of Fake TokenID to Owner addresses
+    
+}
