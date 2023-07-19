cf681a4404b90e25e75f32abb070173a29c63940
Rashford <mikearowo@gmail.com>
2023-07-20 00:22:55 +0100

    new

---
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Replace this line with the Interfaces
+
+contract CryptoDevsDAO is Ownable {
+    /**
+     * Interface for the FakeNFTMarketplace
+     */
+
+    
+}
--- hardhat/contracts/FakeNFTMarketplace.sol
+++ hardhat/contracts/FakeNFTMarketplace.sol
@@ -6,9 +6,9 @@ contract FakeNFTMarketplace {
     /// @dev Maintain a mapping of Fake TokenID to Owner addresses
     mapping(uint256 => address) public tokens;
 
-     /// @dev Set the purchase price for each Fake NFT
-     uint256 nftPrice = 0.1 ether;
-    
+    /// @dev Set the purchase price for each Fake NFT
+    uint256 nftPrice = 0.1 ether;
+
     /// @dev purchase() accepts ETH and marks the owner of the given tokenId as the caller address
     /// @param _tokenId - the fake NFT token Id to purchase
     function purchase(uint256 _tokenId) external payable {
@@ -20,18 +20,15 @@ contract FakeNFTMarketplace {
     function getPrice() external view returns (uint256) {
         return nftPrice;
     }
- /// @dev available() checks whether the given tokenId has already been sold or not
+
+    /// @dev available() checks whether the given tokenId has already been sold or not
     /// @param _tokenId - the tokenId to check for
     function available(uint256 _tokenId) external view returns (bool) {
-
-       // address(0) = 0x0000000000000000000000000000000000000000
+        // address(0) = 0x0000000000000000000000000000000000000000
         // This is the default value for addresses in Solidity
         if (tokens[_tokenId] == address(0)) {
             return true;
-        } return false;
+        }
+        return false;
     }
-
-    
 }
-
-
