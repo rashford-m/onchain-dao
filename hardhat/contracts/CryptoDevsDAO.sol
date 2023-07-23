ea6b1b93dbb2b41cc091184418070cbd2576d4ff
Rashford <mikearowo@gmail.com>
2023-07-23 07:51:25 +0100

    new

--- hardhat/contracts/CryptoDevsDAO.sol
+++ hardhat/contracts/CryptoDevsDAO.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Replace this line with the Interfaces
+interface IFakeNFTMarketplace {
+    /// @dev getPrice() returns the price of an NFT from the FakeNFTMarketplace
+    /// @return Returns the price in Wei for an NFT
+    function getPrice() external view returns (uint256);
+}
