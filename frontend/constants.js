b80d6c2ba6469b4d891ffe88370d0f160d552a8a
Rashford <mikearowo@gmail.com>
2023-08-12 00:10:08 +0100

    new

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+export const CryptoDevsNFTAddress = "0x031a70430e276E4260a217786446D133910D0935"
+export const FakeNFTMarketplaceAddress = "0x7c8cdc810675196565be94df02dcb9bb9cf49f3e"
+export const CryptoDevsDAOAddress = "0x802ec2336af53b272411dc15155eb2020860a13b"
