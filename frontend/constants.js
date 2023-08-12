89b12d77fead6437f5a372d8962d52e3d9439bbd
Rashford <mikearowo@gmail.com>
2023-08-12 12:02:34 +0100

    new

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,3 +1,7 @@
 export const CryptoDevsNFTAddress = "0x031a70430e276E4260a217786446D133910D0935"
 export const FakeNFTMarketplaceAddress = "0x7c8cdc810675196565be94df02dcb9bb9cf49f3e"
 export const CryptoDevsDAOAddress = "0x802ec2336af53b272411dc15155eb2020860a13b"
+
+export const CryptoDevsNFTABI = []; // REPLACE THIS WITH THE NFT CONTRACT ABI
+export const FakeNFTMarketplaceABI = []; // REPLACE THIS WITH THE FAKE MARKETPLACE ABI
+export const CryptoDevsDAOABI = []; // REPLACE THIS WITH THE DAO ABI
