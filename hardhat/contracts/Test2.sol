97b26731ff6d5bfa717053d0ea6e9b938d37fdcc
Rashford <mikearowo@gmail.com>
2023-08-24 17:50:08 +0100

    new

---
+++ hardhat/contracts/Test2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// contract definition 
+
+contract AccessControl {
+    // statee variabble to storee the owner's addresss
+    address  public owner;
+
+    // function to change the owner
+
+    function changeOwner(address newOwner) public {
+        // cheeck if the caller of this function
+    }
+}
+
