dd682f9179082e41398a9755519e3d5bd035f71d
Rashford <mikearowo@gmail.com>
2023-08-24 20:39:30 +0100

    new

--- hardhat/contracts/Test2.sol
+++ hardhat/contracts/Test2.sol
@@ -2,16 +2,20 @@
 
 pragma solidity ^0.8.0;
 
-// contract definition 
+// contract definition
 
 contract AccessControl {
     // statee variabble to storee the owner's addresss
-    address  public owner;
+    address public owner;
 
     // function to change the owner
 
     function changeOwner(address newOwner) public {
-        // cheeck if the caller of this function
+        // check if the caller of this function
+        if (msg.sender == owner) {
+            owner = newOwner; // change the owner to the new address
+        } else {
+            revert("Only the owner can call this function"); // Throw an error if the caller is not the owner
+        }
     }
 }
-
